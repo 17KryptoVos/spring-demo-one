e245083145153d222e166e1e8d5755b953fcfb45
17KryptoVos <17kryptovos@protonmail.com>
2020-07-15 21:24:13 +0200

    - Completed main and added logging

--- src/com/kryptovos/SpringHelloApp.java
+++ src/com/kryptovos/SpringHelloApp.java
@@ -10,9 +10,12 @@ public class SpringHelloApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
 
         // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
 
         // close the context
+        context.close();
     }
 }
