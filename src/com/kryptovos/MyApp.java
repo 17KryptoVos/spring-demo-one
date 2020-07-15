62a17632b8e17667351cb021b9cff6daf854594c
17KryptoVos <17kryptovos@protonmail.com>
2020-07-15 17:19:10 +0200

    - Start of Inversion of Control

--- src/com/kryptovos/MyApp.java
+++ src/com/kryptovos/MyApp.java
@@ -5,11 +5,9 @@ public class MyApp {
     public static void main(String[] args) {
 
         // create the object
-        Coach theFirstCoach = new BaseballCoach();
-        Coach theSecondCoach = new SoccerCoach();
+        Coach theCoach = new BaseballCoach();
 
         // use the object
-        System.out.println(theFirstCoach.getDailyWorkout());
-        System.out.println(theSecondCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyWorkout());
     }
 }
