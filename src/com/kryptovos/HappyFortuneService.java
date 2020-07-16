c0acc66e728e09c319a06f61dd7ce3a6bb12a48d
17KryptoVos <17kryptovos@protonmail.com>
2020-07-16 14:43:39 +0200

    - Start of Dependecy injection

---
+++ src/com/kryptovos/HappyFortuneService.java
@@ -0,0 +1,8 @@
+package com.kryptovos;
+
+public class HappyFortuneService implements FortuneService{
+    @Override
+    public String getFortune() {
+        return "Dont worry be happy";
+    }
+}
